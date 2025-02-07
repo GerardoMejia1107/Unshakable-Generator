9c0014f7cf8381b521958974164ec14d68a79c53
Miguel Mejia <00104923@uca.edu.sv>
2025-02-06 21:12:21 -0600

    🚧 Heading customization

---
+++ src/components/Heading/Heading.jsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+function Heading() {
+    return (
+        <>
+            <header>
+                <section className="title-logo-container">
+                    <img
+                        src="../../assets/icons/orange-logo.png"
+                        alt="company logo"
+                        width="70"
+                    />
+                    <h1>Unshakable</h1>
+                </section>
+            </header>
+        </>
+    );
+}
+export default Heading;
