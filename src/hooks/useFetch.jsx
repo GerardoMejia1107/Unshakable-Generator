487ef35933db996aaea881fb57c27998af67c059
Miguel Mejia <00104923@uca.edu.sv>
2025-02-10 20:29:49 -0600

    🚀 Fetch hook
    Fetch hook created, this will be helpful to deal with random users data for the testimonial carousel

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (!url) return;
+
+        const fetchData = async () => {
+            setLoading(true);
+
+            try {
+                const response = await fetch(url);
+
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+                const data = await response.json();
+                setData(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [url]);
+
+    return { data, loading, error };
+};
+
+export default useFetch;
