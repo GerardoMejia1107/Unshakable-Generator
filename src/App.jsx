beed66f6962fd2036cfe3c69f9354fdbce4071d4
Miguel Mejia <00104923@uca.edu.sv>
2025-02-11 14:09:37 -0600

    🚀 Initial code for the carousel, other colors
    Initial html code for the carousel, this cards added will be turn into a card component

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,17 @@ import Generator from "./components/Generator/Generator";
 import FeatureList from "./components/FeaturesList/FeaturesList";
 
 import { featuresData } from "./constants/featuresData.js";
+import useFetch from "./hooks/useFetch.jsx";
 
 function App() {
+    const { data, loading, error } = useFetch(
+        "https://randomuser.me/api/?results=1"
+    );
+
+    function handleGenerateUser() {}
+
+    handleGenerateUser();
+
     return (
         <>
             <Heading />
@@ -16,6 +25,10 @@ function App() {
             <section id="features-section">
                 <h2>Why you should choose us?</h2>
                 <FeatureList featuresData={featuresData} />
+            </section>
+
+            <section id="testimonial__carousel__section">
+
             </section>
         </>
     );
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,77 @@
 import "./styles.css";
 
 function Carousel() {
-    return <></>;
+
+    return <>
+        <h2>What Our Customers Say</h2>
+        <i id="left" className="ri-arrow-left-wide-fill"></i>
+
+        <ul className="carousel">
+            <li className="card">
+                <div className="picture-container">
+                    <img src="" alt="" draggable="false"/>
+                </div>
+                <h3 className="user-name"></h3>
+                <h4 className="user-profession"></h4>
+                <p className="user-testimonial-message"></p>
+            </li>
+
+            <li className="card">
+                <div className="picture-container">
+                    <img src="" alt="" draggable="false"/>
+                </div>
+                <h3 className="user-name"></h3>
+                <h4 className="user-profession"></h4>
+                <p className="user-testimonial-message"></p>
+            </li>
+
+            <li className="card">
+                <div className="picture-container">
+                    <img src="" alt="" draggable="false"/>
+                </div>
+                <h3 className="user-name"></h3>
+                <h4 className="user-profession"></h4>
+                <p className="user-testimonial-message"></p>
+            </li>
+
+            <li className="card">
+                <div className="picture-container">
+                    <img src="" alt="" draggable="false"/>
+                </div>
+                <h3 className="user-name"></h3>
+                <h4 className="user-profession"></h4>
+                <p className="user-testimonial-message"></p>
+            </li>
+
+            <li className="card">
+                <div className="picture-container">
+                    <img src="" alt="" draggable="false"/>
+                </div>
+                <h3 className="user-name"></h3>
+                <h4 className="user-profession"></h4>
+                <p className="user-testimonial-message"></p>
+            </li>
+
+            <li className="card">
+                <div className="picture-container">
+                    <img src="" alt="" draggable="false"/>
+                </div>
+                <h3 className="user-name"></h3>
+                <h4 className="user-profession"></h4>
+                <p className="user-testimonial-message"></p>
+            </li>
+
+            <li className="card">
+                <div className="picture-container">
+                    <img src="" alt="" draggable="false"/>
+                </div>
+                <h3 className="user-name"></h3>
+                <h4 className="user-profession"></h4>
+                <p className="user-testimonial-message"></p>
+            </li>
+        </ul>
+        <i className="ri-arrow-right-wide-fill"></i>
+    </>;
 }
 
 export default Carousel;
