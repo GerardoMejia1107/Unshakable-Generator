bb490dbff3b2da07af51526e67a9488a4b5e988b
Miguel Mejia <00104923@uca.edu.sv>
2025-02-13 20:20:58 -0600

    ✨ Adding FAQs
    Initial style and disign for FAQs items

--- src/FAQsList/FAQsItem/FAQsItem.jsx
+++ src/FAQsList/FAQsItem/FAQsItem.jsx
@@ -1,9 +1,28 @@
 /* eslint-disable react/prop-types */
+import "./styles.css";
+import { useState } from "react";
 function FAQsItem({ FAQ }) {
+    const [isExpanded, setIsExpanded] = useState(false);
+    function expandContent() {
+        setIsExpanded(!isExpanded);
+    }
     return (
         <li className="FAQs__item">
-            <div className="question">{FAQ.question}</div>
-            <div className="answer">{}</div>
+            <div
+                className="question"
+                onClick={expandContent}
+                aria-expanded={isExpanded}
+            >
+                <h4>{FAQ.question}</h4>
+                {!isExpanded ? (
+                    <i className="ri-add-circle-line"></i>
+                ) : (
+                    <i className="ri-subtract-line"></i>
+                )}
+            </div>
+            <div className={`answer ${isExpanded ? "expand" : ""}`}>
+                {<p>{FAQ.answer}</p>}
+            </div>
         </li>
     );
 }
